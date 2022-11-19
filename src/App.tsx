af8e3c5d7fc5de94b29e389072c43bd90c7a2248
Justin Williams <justinwilliams42@gmail.com>
2022-11-19 13:02:44 -0800

    under construction

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
-import { MantineProvider, Text, Button, Stack } from "@mantine/core";
-import { theme } from "./theme";
+import { MantineProvider, Stack, Text, Title } from '@mantine/core'
+import { theme } from './theme'
 
 export default function App() {
   return (
     <MantineProvider theme={theme} withGlobalStyles withNormalizeCSS>
       <Stack align="center" mt={50}>
-        <Text size="xl" weight={500}>
-          Welcome to Mantine!
-        </Text>
-        <Button>Click the button</Button>
+        <Title>
+          Welcome to{' '}
+          <Text
+            component="span"
+            variant="gradient"
+            gradient={{ from: 'purple', to: 'blue' }}>
+            Han & Justin's
+          </Text>{' '}
+          wedding site. ❤️
+        </Title>
+        <Text size="xl">🚧 Under construction. Check back soon! 🚧</Text>
       </Stack>
     </MantineProvider>
-  );
+  )
 }
